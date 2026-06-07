757e047b4ae63a40d4c2dd877597655cf152c053
DotBox <aadi2399@outlook.com>
2026-06-07 14:29:02 +0530

    window manager can now load roms through drag and drop, the manager also renders game display at 60Hz using polled hardware display state

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,6 @@ int main(int argc, char* argv[]) {
 
     // Initialize system subcomponents;
     chip8::System system;
-
-    // Parse command line arguments to get the binary path 
-    if (argc != 2) {
-        EPRINTF("FATAL: Invalid argument list.\n");
-        std::exit(-1);
-    }
-
-    std::string romPath = argv[1];
-
-    // Load the binary into Chip8 memory
-    system.loadRom(romPath.c_str());
     
     // run the binary on the VM
     system.run();
